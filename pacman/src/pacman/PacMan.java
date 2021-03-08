16914748e7e06ff1522fc2e3c207b282fa60d8b3
muhammed-enes-koroglu <menes2000@hotmail.com>
2021-03-08 13:38:46 +0100

    PacMan implemented and tested succesfully.

--- pacman/src/pacman/PacMan.java
+++ pacman/src/pacman/PacMan.java
@@ -2,22 +2,79 @@ package pacman;
 
 /**
  * Each instance of this class represents the player-controlled Pac-Man character in a Pac-Man maze.
+ * 
+ * @mutable
+ * 
+ * @invar | getSquare() != null
+ * @invar | getNbLives() >= 0
  */
 public class PacMan {
 	
-	public Square getSquare() { throw new RuntimeException("Not yet implemented"); }
+	/**
+	 * @invar | square != null
+	 * @invar | nbLives >= 0
+	 */
+	private int nbLives;
+	/** @representationObject */
+	private Square square;
 	
-	public int getNbLives() { throw new RuntimeException("Not yet implemented"); }
-
-	public PacMan(int nbLives, Square square) {
-		throw new RuntimeException("Not yet implemented");
+	/** @basic */	
+	public Square getSquare() { 
+		return square;
 	}
 	
-	public void setSquare(Square square) { throw new RuntimeException("Not yet implemented"); }
+	/** @basic */
+	public int getNbLives() { 
+		return nbLives;
+	}
+
+	/**
+	 * @throws IllegalArgumentException if the given {@code nbLives} not greater than zero
+	 * 		| nbLives <= 0
+	 * @throws IllegalArgumentException if the given {@code square} is null
+	 * 		| square == null
+	 * 
+	 * @post | getSquare().equals(square)
+	 * @post | getNbLives() == nbLives
+	 */
+	public PacMan(int nbLives, Square square) {
+		if (nbLives <= 0)
+			throw new IllegalArgumentException("Given number of lives must be greater than zero");
+		if (square == null)
+			throw new IllegalArgumentException("given square is null");
+
+		this.nbLives = nbLives;
+		this.square = square;
+	}
+	
+	/**
+	 * @mutates | this
+	 * 
+	 * @throws IllegalArgumentException | square == null
+	 * 
+	 * @post | getSquare().equals(square)
+	 */
+	public void setSquare(Square square) { 
+		if (square == null)
+			throw new IllegalArgumentException("given square is null");
+		
+		this.square = square;
+	}
 	
 	/**
 	 * Decreases this Pac-Man character's number of lives by one.
+	 * 
+	 * @inspects | this
+	 * @mutates | this
+	 * 
+	 * @throws Exception | getNbLives() == 0
+	 * @post | getNbLives() == old(getNbLives()) - 1
+	 * 
 	 */
-	public void die() { throw new RuntimeException("Not yet implemented"); }
+	public void die() { 
+		if (getNbLives() == 0)
+			throw new IllegalArgumentException("Number of lives is already zero");
+		nbLives--;
+	}
 
 }
---
+++ pacman/src/pacman/tests/PacManTest.java
@@ -0,0 +1,34 @@
+package pacman.tests;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+import pacman.MazeMap;
+import pacman.PacMan;
+import pacman.Square;
+
+class PacManTest {
+
+	@Test
+	void test() {
+		boolean[] passable = {false, false, true,  false,
+				  			  false, false, true,  true,
+				  			  false, false, false, true};
+		int mapColumns = 4, mapRows = 3;
+		MazeMap map = new MazeMap(mapColumns, mapRows, passable);
+		
+		Square s1 = Square.of(map, 1,2);
+		Square s2 = Square.of(map, 0,2);
+		int lives = 3;
+		PacMan pacman = new PacMan(lives, s1);
+		assert(pacman.getSquare().equals(s1));
+		assert(pacman.getNbLives() == lives);
+		pacman.setSquare(s2);
+		assert(pacman.getSquare().equals(s2));
+		pacman.die();
+		assert(pacman.getNbLives() == lives - 1);
+		
+	}
+
+}
